de4041c565322e58636c2ba618ae0a006d661b52
병국 <ppusarida@gmail.com>
2015-04-15 18:37:31 +0900

    라이프 사이클

--- src/main/java/di/lifeCycle/MainClass.java
+++ src/main/java/di/lifeCycle/MainClass.java
@@ -8,15 +8,19 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		
+		// 생성
 		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
 		
+		// 설정
 		ctx.load("classpath:applicationCTX.xml");
 		ctx.refresh();
 		
+		// 사용
 		Student student = ctx.getBean("student1", Student.class);
 		System.out.println("이름: " + student.getName());
 		System.out.println("나이: " + student.getAge());
 		
+		// 소멸
 		ctx.close();		
 	}
 }
