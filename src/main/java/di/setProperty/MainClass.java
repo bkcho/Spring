6c98b2aa96e0bcba6d4c3e3576f5b790e4468e92
병국 <ppusarida@gmail.com>
2015-04-15 22:59:34 +0900

    Environment를 사용하지 않고 바로 java 코드를 이용하여 사용하는 방법

---
+++ src/main/java/di/setProperty/MainClass.java
@@ -0,0 +1,19 @@
+package di.setProperty;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class MainClass {
+	
+	public static void main(String[] args) {
+		
+		AbstractApplicationContext ctx = new GenericXmlApplicationContext("classpath:applicationCTX_setProperty.xml");
+		
+		AdminConnection connection = ctx.getBean("adminConnection", AdminConnection.class);
+		System.out.println("adminID: " + connection.getAdminId());
+		System.out.println("adminPW: " + connection.getAdminPw());
+		System.out.println("sub_adminID: " + connection.getSub_adminId());
+		System.out.println("sub_adminPW: " + connection.getSub_adminPw());		
+	}
+
+}
