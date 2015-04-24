d08c014474c07311324e6e6473c98a0a7617e09d
병국 <ppusarida@gmail.com>
2015-04-24 23:33:41 +0900

    aop 다양한 기능 정리

--- src/main/java/aop/exam2/LogAop.java
+++ src/main/java/aop/exam2/LogAop.java
@@ -8,7 +8,16 @@ import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class LogAop {
+/*	
+	@Pointcut("execution(public void get*(..))") // public void인 모든 get메소드
+	@Pointcut("execution(* aop.exam2.*.*())") // aop.exam2 패키지에 파라미터가없는 모든 메소드	
+	@Pointcut("execution(* aop.exam2..*.*())") // aop.exam2 패키지 & aop.exam2 하위패키지에 파라미터가 없는 모든 메소드	
+	@Pointcut("execution(* aop.exam2.Worker.*())") // aop.exam2.Worker 모든 메소드
 	
+	@Pointcut("within(aop.exam2.*)") // aop.exam2 패키지 안에 있는 모든 메소드
+	@Pointcut("within(aop.exam2..*)") // aop.exam2 패키지 및 하위 패키지 안에 있는 모든 메소드			
+	@Pointcut("within(aop.exam2.Worker)") // aop.exam2.Worker 모든 메소드				
+*/			
 	@Pointcut("within(aop.exam2.*)")
 	private void pointcutMethod(){ 		
 	}
